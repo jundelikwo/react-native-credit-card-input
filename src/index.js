48e0960cafdad5d7d5bbc748ea8712c7691dcae0
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-05-24 20:02:48 +0100

    Export CCInput component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import connectToState from "./connectToState";
 import CCF from "./CreditCardInput";
+import CCI from "./CCInput";
 import LiteCCF from "./LiteCreditCardInput";
 import CV from "./CardView";
 
 export const CreditCardInput = connectToState(CCF);
 export const LiteCreditCardInput = connectToState(LiteCCF);
+export const CCInput = CCI;
 export const CardView = CV;
